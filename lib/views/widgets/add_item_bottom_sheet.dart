002689f6b65bbcc52e3cc87902cd54956a17be3f
Tarek3222 <00tarek404@gmail.com>
2024-07-21 16:06:04 +0300

    add add note cubit to add note bootom sheet

--- lib/views/widgets/add_item_bottom_sheet.dart
+++ lib/views/widgets/add_item_bottom_sheet.dart
@@ -1,14 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
+import 'package:notes_app/cubits/add_note_cubit/add_note_cubit.dart';
+import 'package:notes_app/cubits/add_note_cubit/add_note_state.dart';
 import 'package:notes_app/views/widgets/add_note_form.dart';
+
 class AddItemBottomSheet extends StatelessWidget {
   const AddItemBottomSheet({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const  Padding(
-      padding:  EdgeInsets.symmetric(vertical: 24,horizontal: 16),
-      child:  SingleChildScrollView(
-        child:AddNoteForm() ,
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 24, horizontal: 16),
+      child: SingleChildScrollView(
+        child: BlocConsumer<AddNoteCubit, AddNotesState>(
+          listener: (context, state) {
+            if (state is AddNoteFailure) {
+              ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(
+                  content: Text(state.errorMessage),
+                ),
+              );
+            }
+            if (state is AddNoteSuccess) {
+              Navigator.pop(context);
+            }
+          },
+          builder: (context, state) {
+            return ModalProgressHUD(
+              inAsyncCall: state is AddNoteLoading ? true : false,
+              child: const AddNoteForm(),
+            );
+          },
+        ),
       ),
     );
   }
