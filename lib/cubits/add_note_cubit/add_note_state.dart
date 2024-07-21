68cc3350b6d3f3b76b420056d04c5c5c97e834b0
Tarek3222 <00tarek404@gmail.com>
2024-07-21 15:08:41 +0300

    add addNote method to add note cubit

--- lib/cubits/add_note_cubit/add_note_state.dart
+++ lib/cubits/add_note_cubit/add_note_state.dart
@@ -4,5 +4,5 @@ class AddNoteLoading extends AddNotesState{}
 class AddNoteSuccess extends AddNotesState{}
 class AddNoteFailure extends AddNotesState{
   final String errorMessage;
-  AddNoteFailure({required this.errorMessage});
+  AddNoteFailure (this.errorMessage);
 }
