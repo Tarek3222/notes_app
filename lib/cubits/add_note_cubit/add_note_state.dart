3c520f9b1907cda3794373baa35d6649b030dde5
Tarek3222 <00tarek404@gmail.com>
2024-07-21 14:46:28 +0300

    register adapter

--- lib/cubits/add_note_cubit/add_note_state.dart
+++ lib/cubits/add_note_cubit/add_note_state.dart
@@ -2,4 +2,7 @@ class AddNotesState{}
 class AddNoteInitial extends AddNotesState{}
 class AddNoteLoading extends AddNotesState{}
 class AddNoteSuccess extends AddNotesState{}
-class AddNoteFailure extends AddNotesState{}
+class AddNoteFailure extends AddNotesState{
+  final String errorMessage;
+  AddNoteFailure({required this.errorMessage});
+}
